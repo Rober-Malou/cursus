39cc0cc78557259c90c8fd83857eaa664cbf595e
Roberto Benitez Martin <robenite@c1r13s2.42madrid.com>
2024-10-07 22:18:18 +0200

    fin 07/10

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,15 +6,31 @@
 /*   By: robenite <robenite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 21:37:59 by robenite          #+#    #+#             */
-/*   Updated: 2024/10/06 22:48:02 by robenite         ###   ########.fr       */
+/*   Updated: 2024/10/07 19:56:31 by robenite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-char **ft_split(char const *s, char c)
+static int	ft_nsplit(char *str, char c)
 {
-    char    full;
+	int	i;
+	int	count;
 
-    full = ft_calloc(s, sizeof (char));
+	count = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	*split;
+
+	split = ft_calloc(ft_nsplit(s, c), sizeof(char));
 }
