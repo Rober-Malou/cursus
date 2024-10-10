70d6eea28896991f96f44fba3d0ae743b2bba62f
Roberto <benitezmartinroberto@gmail.com>
2024-10-10 11:51:48 +0200

    10/10

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: robenite <robenite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 21:37:59 by robenite          #+#    #+#             */
-/*   Updated: 2024/10/07 22:20:52 by robenite         ###   ########.fr       */
+/*   Updated: 2024/10/09 11:46:52 by robenite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,24 +28,46 @@ static int	ft_nsplit(char *str, char c)
 	return (count);
 }
 
-char *ft_wordscopy()
+static int	*ft_wordlen(char const *str, char c, int p)
 {
+	int	len;
 
+	len = 0;
+	while (str[p] != c)
+		len++;
+	return (len);
+}
+
+char	*ft_wordscopy(char const *str, char c, int start, int finish)
+{
+	char	*word;
+
+	word = (char *)malloc(finish - start);
+	return (word);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	*split;
-	int	i;
-	int	j;
-	int	words;
+	int		i;
+	int		words;
+	int		finish;
+	int		start;
 
 	split = ft_calloc(ft_nsplit(s, c), sizeof(char));
 	i = 0;
-	j = 0;
 	words = ft_nsplit;
+	start = 0;
+	finish = (ft_wordlen(s, c, start));
 	while (i <= words)
 	{
-		split[i] = ft_wordscopy();
+		split[i] = ft_wordscopy(s, c, start, finish);
+		while (split[start] <= split[finish])
+		{
+			split == s[start];
+			start++;
+		}
+		i++;
 	}
+	return (split);
 }
