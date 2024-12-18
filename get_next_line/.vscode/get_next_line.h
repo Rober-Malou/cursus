f25f37eb62f27b82892e6b3df4434427dae3e5c9
Roberto <benitezmartinroberto@gmail.com>
2024-12-19 00:20:17 +0100

    19/12

--- get_next_line/.vscode/get_next_line.h
+++ get_next_line/.vscode/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: robenite <robenite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 01:38:32 by robenite          #+#    #+#             */
-/*   Updated: 2024/12/16 16:39:39 by robenite         ###   ########.fr       */
+/*   Updated: 2024/12/17 03:26:16 by robenite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,15 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
-
 # include <fcntl.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s);
-size_t	ft_strlen(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*get_next_line(int fd);
-char	*check_line(int fd, char *buf, ssize_t *bytes_read, int *i);
-char	*join_pieces(char *piece_of_line);
-char	*going_free(char **object);
-
+char		*ft_strchr(const char *s, int c);
+char		*ft_strdup(const char *s);
+size_t		ft_strlen(const char *s);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*get_next_line(int fd);
+char		*get_next_line(int fd);
 #endif
