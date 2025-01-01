5bdbd2d3d57c6d0744a160ad8f4e0142279829a7
Roberto <benitezmartinroberto@gmail.com>
2025-01-01 05:32:34 +0100

    31/12

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,11 +6,16 @@
 /*   By: robenite <robenite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 21:31:53 by robenite          #+#    #+#             */
-/*   Updated: 2024/12/30 03:48:50 by robenite         ###   ########.fr       */
+/*   Updated: 2025/01/01 05:31:03 by robenite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	format_chek(char format, va_list args);
+static int	hexa_f(unsigned long long args, char *base, int c);
+static int	float_f(float args);
+static int	ft_putnbr_ish_no_sign_fd(int args);
 
 int	ft_printf(char const *text, ...)
 {
@@ -29,10 +34,7 @@ int	ft_printf(char const *text, ...)
 			return_counter += format_chek(text[control_counter], args);
 		}
 		else
-		{
-			ft_putchar_fd(text[control_counter], 1);
-			return_counter++;
-		}
+			return_counter += ft_putchar_ish_fd(text[control_counter], 1);
 		control_counter++;
 	}
 	va_end(args);
@@ -45,96 +47,90 @@ static int	format_chek(char format, va_list args)
 
 	r_counter = 0;
 	if (format == 'c')
-		r_counter = write(1, args, 1);
+		r_counter = ft_putchar_ish_fd(va_arg(args, int), 1);
 	else if (format == 's')
-		r_counter = formater_one(format, args);
+		r_counter = ft_putstr_ish_fd(va_arg(args, char *), 1);
 	else if (format == 'p')
-		r_counter = hexa_min(args, "0123456789abcdef", 1);
+		r_counter = hexa_f(va_arg(args, unsigned long long), "0123456789abcdef",
+				1);
 	else if (format == 'd')
-		r_counter = formater_number(format, args)
-	else if (format == 'i') 
-		r_counter = 
-	else if (format == 'u') 
-		r_counter = 
-	else if (format == 'x') 
-		r_counter = 
-	else if (format == 'X') 
-		r_counter = 
+		r_counter = float_f(va_arg(args, double));
+	else if (format == 'i')
+		r_counter = ft_putnbr_ish_fd(va_arg(args, int), 1);
+	else if (format == 'u')
+		r_counter = ft_putnbr_ish_no_sign_fd(va_arg(args, int));
+	else if (format == 'x')
+		r_counter = hexa_f(va_arg(args, unsigned long long), "0123456789abcdef",
+				1);
+	else if (format == 'X')
+		r_counter = hexa_f(va_arg(args, unsigned long long), "0123456789ABCDEF",
+				1);
 	else if (format == '%')
-		r_counter = write(1, '%', 1);
+		r_counter = ft_putchar_ish_fd(va_arg(args, int), 1);
 	return (r_counter);
 }
 
-static int	formater_one(char format, va_list args)
-{
-	int	sum;
-
-	if (format == 's')
-	{
-		while (args)
-		{
-			sum = ft_strlen(args);
-			ft_putstr_fd(args, 1);
-		}
-	}
-	return (sum);
-}
-
-static int	hexa_min(unsigned long long args, char *base, int c)
+static int	hexa_f(unsigned long long args, char *base, int c)
 {
 	int	sum;
 
 	sum = 0;
 	if (!args)
 	{
-		sum += ft_print_str("(nil)");
+		sum = ft_putstr_ish_fd("NULL", 1);
 		return (sum);
 	}
 	if (c == 1)
 	{
-		sum += write(1, "0x", 2);
+		sum += ft_putstr_ish_fd("0x", 1);
 		c = 0;
 	}
 	while (args >= 16)
-		sum += hexa_min(args / 16, base, c);
-	sum += write(1, &base[args % 16], 1);
+		sum += hexa_f(args / 16, base, c);
+	sum += ft_putstr_ish_fd(&base[args % 16], 1);
 	return (sum);
 }
 
-static int	formater_number(char format, va_list args)
+static int	float_f(float args)
 {
-	int	sum;
-
-	if (format == "i")
-	{
-		ft_putnbr_fd(args, 1);
-		while (args > 0)
-		{
-			args = (int) args / 10;
-			sum ++;
-		}
-	}
-	else if (format == "d")
-	{
-
-	}
-}
-
-static int format_loat(float args)
-{
-	int	control;
+	int		sum;
+	int		control;
 	float	left_over;
 
+	sum = 0;
 	control = (int)args;
 	left_over = args;
-	ft_putnbr_fd(args, 1);
-	write(1, ".", 1);
-	left_over - control;
+	sum += ft_putnbr_ish_fd(args, 1);
+	sum += ft_putchar_ish_fd('.', 1);
+	left_over = left_over - control;
 	while (left_over > 0)
 	{
-		left_over * 10;
-		ft_putnbr_fd(left_over, 1);
+		left_over = left_over * 10;
+		sum += ft_putnbr_ish_fd(left_over, 1);
 		left_over = control;
 		left_over = left_over - control;
 	}
+	return (sum);
+}
+
+static int	ft_putnbr_ish_no_sign_fd(int args)
+{
+	char	c;
+	int		count;
+
+	count = 0;
+	if (args == -2147483648)
+	{
+		count += write(1, "2147483648", 10);
+		return (count);
+	}
+	if (args < 0)
+		args = -args;
+	if (args >= 10)
+	{
+		ft_putnbr_ish_no_sign_fd(args / 10);
+	}
+	c = (args % 10) + '0';
+	count += write(1, &c, 1);
+	return (count);
 }
--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,16 +6,20 @@
 /*   By: robenite <robenite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 00:32:14 by robenite          #+#    #+#             */
-/*   Updated: 2024/12/30 00:59:29 by robenite         ###   ########.fr       */
+/*   Updated: 2025/01/01 05:04:21 by robenite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <stdarg.h>
-
-#ifdef FT_PRINTF_H
+#ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-int	ft_printf(char const *text, ...);
+int		ft_printf(char const *text, ...);
+int		ft_putchar_ish_fd(char c, int fd);
+size_t	ft_strlen(const char *s);
+int		ft_putstr_ish_fd(char *s, int fd);
+int		ft_putnbr_ish_fd(int n, int fd);
 
 #endif
