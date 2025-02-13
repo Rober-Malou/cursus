f4cd994ae74a37b3b43c82aa4369a56217abe40f
Roberto Benitez Martin <robenite@c2r4s6.42madrid.com>
2025-02-13 20:11:08 +0100

    13/02/25

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,20 @@
 /*   By: robenite <robenite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 21:33:23 by robenite          #+#    #+#             */
-/*   Updated: 2025/02/03 21:33:29 by robenite         ###   ########.fr       */
+/*   Updated: 2025/02/13 19:31:05 by robenite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int	main(int argc, char *argv[])
+{
+	int	n;
+
+	n = 0;
+	while (n < argc)
+	{
+		t_list *ft_lstnew(argv[n]);
+		n++;
+	}
+}
